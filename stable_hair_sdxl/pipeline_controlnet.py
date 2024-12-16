253121c49fb00c585ef9c977e4b4fe3f43b2b4c8
curt-park <www.jwpark.co.kr@gmail.com>
2024-12-17 01:25:36 +0900

    Fix vae scale factor

--- stable_hair_sdxl/pipeline_controlnet.py
+++ stable_hair_sdxl/pipeline_controlnet.py
@@ -4,11 +4,11 @@ from diffusers import StableDiffusionXLControlNetPipeline
 class StableHairSDXLControlNetPipeline(StableDiffusionXLControlNetPipeline):
 
     def prepare_image(self, image, **kwargs):
-        # the image is downsized by `prepare_image`.
-        self.vae_scale_factor = 1
         image = super().prepare_image(image, **kwargs)
         # norm
         image = 2.0 * image - 1.0
         image = self.vae.encode(image).latent_dist.sample()
         image = image * self.vae.config.scaling_factor
+        # the image is downsized by `prepare_image`.
+        self.vae_scale_factor = 1
         return image
