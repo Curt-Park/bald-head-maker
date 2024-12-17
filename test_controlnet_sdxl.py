2e78983cd8fb2bed73d7602913c8eb9c2036cafe
curt-park <www.jwpark.co.kr@gmail.com>
2024-12-17 09:00:23 +0900

    Change loading method

--- test_controlnet_sdxl.py
+++ test_controlnet_sdxl.py
@@ -1,26 +1,26 @@
 import argparse
 import torch
 
-from diffusers import UniPCMultistepScheduler
+from diffusers import UNet2DConditionModel, UniPCMultistepScheduler
 from diffusers.utils import load_image
 
 from stable_hair_sdxl.controlnet import StableHairControlNetModel
 from stable_hair_sdxl.pipeline_controlnet import StableHairSDXLControlNetPipeline
 
 
-parser = argparse.ArgumentParser(description='스테이블 디퓨전 헤어 변환 파이프라인')
+parser = argparse.ArgumentParser(description="스테이블 디퓨전 헤어 변환 파이프라인")
 parser.add_argument(
-    '--pretrained-model-path',
+    "--pretrained-model-path",
     type=str,
     default="stabilityai/stable-diffusion-xl-base-1.0",
 )
 parser.add_argument(
-    '--controlnet-model-path',
+    "--controlnet-model-path",
     type=str,
     required=True,
 )
 parser.add_argument(
-    '--input-image',
+    "--input-image",
     type=str,
     required=True,
 )
@@ -28,7 +28,12 @@ args = parser.parse_args()
 device = "cuda" if torch.cuda.is_available else "cpu"
 
 
-bald_converter = StableHairControlNetModel.from_pretrained(args.controlnet_model_path).to(device)
+unet = UNet2DConditionModel.from_pretrained(
+    args.pretrained_model_path, subfolder="unet"
+)
+bald_converter = StableHairControlNetModel.from_unet(unet)
+bald_converter.load_state_dict(torch.load(args.controlnet_model_path))
+bald_converter.to(device)
 remove_hair_pipeline = StableHairSDXLControlNetPipeline.from_pretrained(
     args.pretrained_model_path,
     controlnet=bald_converter,
@@ -44,5 +49,7 @@ control_image = load_image(args.input_image).resize((1024, 1024))
 
 # generate image
 generator = torch.manual_seed(0)
-image = remove_hair_pipeline("", num_inference_steps=20, generator=generator, image=control_image).images[0]
+image = remove_hair_pipeline(
+    "", num_inference_steps=20, generator=generator, image=control_image
+).images[0]
 image.save("./output.png")
